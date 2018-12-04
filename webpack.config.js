99fa84767e4499c4c4c0a57bb55d06e115a57465
Justin Lafferty <justin.lafferty@walmart.com>
2018-12-04 08:46:06 -0600

    added copying of assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -19,7 +20,8 @@ module.exports = {
     new CleanWebpackPlugin(['build']),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }])
   ],
   module: {
     rules: [
